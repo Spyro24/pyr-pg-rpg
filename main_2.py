f05649b4062ba79b128a6a690eafab0d8890703c
Spyro24 <spyro24@web.de>
2024-03-02 14:05:32 +0100

    ready to push

--- main_2.py
+++ main_2.py
@@ -98,10 +98,29 @@ while run:
                     game = False
                     run = False
                     
-            #WASD key Script
-            #if 
             #get all pressed keys
             key_ar = list(p.key.get_pressed())
+            
+            #WASD key Script
+            if wasd_act:
+                if key_ar[3+23] and key_ar[3+1]:
+                    direction = "LUP"
+                elif key_ar[3+23] and key_ar[3+4]:
+                    direction = "RUP"
+                elif key_ar[3+1] and key_ar[3+19] :
+                    direction = "LWN"
+                elif key_ar[3+4]  and key_ar[3+19]:
+                    direction = "RWN"
+                
+                #This was the Original control script
+                elif key_ar[3+23]:
+                    direction = "UP"
+                elif key_ar[3+1]:
+                    direction = "LEFT"
+                elif key_ar[3+19]:
+                    direction = "DOWN"
+                elif key_ar[3+4]:
+                    direction = "RIGHT"
             
             #load a map
             if load_map:
