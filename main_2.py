feaad2884c6afd7914626bbc7dd114994edab59b
Spyro24 <spyro24@web.de>
2024-03-08 20:34:28 +0100

    add some comments to main_2.py and change the code style

--- main_2.py
+++ main_2.py
@@ -34,9 +34,8 @@ speed_glob = 0.5
 mpx, mpy = init_val[6]
 
 #internal vars(DO NOT CHANGE)
-run = True; update = False; counter = 0; cur_map = rpg_map.map_load(init_val[6],(16,16),2); load_map = False; next_map = None; blit_map = False
-title_screen = False; next_map = None; hit_on_map = False; game = True; choser = 0; block_time = 0.0; ar_key_time = 0; p_moved = False; reset_save = False
-
+update = False; counter = 0; load_map = False; next_map = None; blit_map = False; title_screen = False; next_map = None; hit_on_map = False;
+game = True; choser = 0; block_time = 0.0; ar_key_time = 0; p_moved = False; reset_save = False; wasd_act = True
 
 #init commands
 p.init(); gw = p.display.set_mode((int(init_val[2]) * int(init_val[3]), int(init_val[2]) * int(init_val[3]))); p.display.set_caption(init_val[0])
@@ -46,14 +45,18 @@ gww = int(init_val[2]) * int(init_val[3]); gwh = int(init_val[2]) * int(init_val
 #main function
 run = True
 title_screen = True
+
+#uncomment this lines for beta test
+title_screen = False #execute the titlescreen loop
+game = True #execute the game loop
+
+
 while run:
-    #check for the title screen
-    if title_screen:
-        #init the title screen vars
-        update = True
+    if title_screen: #check for the title screen
+        update = True #Refresh the screen
         pos = 0 #The position of the pointer
         background = p.image.load("./images/back.png") #load the title background
-        while title_screen:
+        while title_screen: #titlescreen loop
             events_ = p.event.get()
             p.event.clear()
             for event in events_:
@@ -61,27 +64,13 @@ while run:
                     run = False
                     title_screen = False
             
-            #get the cur pressed keys
-            key_ar = list(p.key.get_pressed())
-            '''
-            #key test code
-            n = 0
-            for test in key_ar:
-                if test:
-                    print(n)
-                n += 1
-            '''
-            if update:
+            key_ar = list(p.key.get_pressed()) #Aray with all keys
+            if update: #Update the Display
                 gw.blit(background,(0,0))
                 pwn.draw_rect(gw, 0, (gwh / 8 * 4), gww, (gwh / 8 * 3), (0,127,255))
                 pwn.draw_rect(gw, 0, (gwh / 8 * (4 + int(choser))), gww, (gwh / 8 * 1), (255,42,0))
                 p.display.flip()
                 update = False
-            
-            '''
-            if (time_get() - ar_key_time) > speed_glob:
-                pass
-            '''
             
             if (key_ar[3+19] or key_ar[3+23]) == False:
                 block_time = 0
@@ -105,10 +94,10 @@ while run:
             
             if key_ar[44]:
                 if choser == 0:
+                    pass
                     
-        
-            
-    if game:
+                
+    if game: #start Game loop
         while game:
             
             #we can call event.get() once in a run
@@ -122,41 +111,38 @@ while run:
             
             #WASD key Script
             if wasd_act:
+                direction = None
                 if key_ar[3+23] and key_ar[3+1]:
-                    direction = "LUP"
+                    direction = "LUP"                    
                 elif key_ar[3+23] and key_ar[3+4]:
-                    direction = "RUP"
+                    direction = "RUP"                    
                 elif key_ar[3+1] and key_ar[3+19] :
-                    direction = "LWN"
+                    direction = "LWN"                    
                 elif key_ar[3+4]  and key_ar[3+19]:
-                    direction = "RWN"
-                
-                #This was the Original control script
+                    direction = "RWN"                
                 elif key_ar[3+23]:
-                    direction = "UP"
+                    direction = "UP"                    
                 elif key_ar[3+1]:
-                    direction = "LEFT"
+                    direction = "LEFT"                    
                 elif key_ar[3+19]:
                     direction = "DOWN"
                 elif key_ar[3+4]:
                     direction = "RIGHT"
-            
-            #load a map
-            if load_map:
+                    
+            if load_map: #load the map
                 if hit_on_map:
                     nex_map = rpg_map.map_load((x_m, y_m), (16,16), 2)
                 else:
                     cur_map = rpg_map.map_load((x_m, y_m), (16,16), 2)
                 load_map = False
                 
-            #show curent map
-            if blit_map:
+            if blit_map: #show curent map
                 game.fill("black")
                 rpg_map.map_blit(game,mapx,mapy,cur_map,tl_size)
                 update = True
                 blit_map = False
                 
-            if p_moved:
+            if p_moved: #get the move state of the player
                 pass
             
             #update the display
@@ -164,11 +150,11 @@ while run:
                 p.display.flip()
                 update = False
             
-            
             #update the programm counter
             counter += 1
     
-    if reset_save:
+    
+    if reset_save: #start reset loop to reset the save
         while reset_save:
             if update:
                 p.display.flip()
