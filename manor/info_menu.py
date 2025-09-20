42e1474fdfbdd53bd0508e74b8ba1d70fff1a0b6
Spyro24 <minerpi16@gmail.com>
2025-09-20 23:16:55 +0200

    finished info menu

--- manor/info_menu.py
+++ manor/info_menu.py
@@ -8,17 +8,19 @@ class infoMenu:
         self.cache = container.cache
         self.debug = container.debugMode
         self.window = container.window
-        self.font = container.font
+        self.font: pyr_pg.font.font = container.font
         self.lastFunction = None
+        self.textList = self.__read_info()
         self.setup()
         
     def setup(self):
         self.lowestSize = self.window.lowestSize
         self.menuSize = self.lowestSize / 10
-        self.fontSize = self.menuSize * 0.8
+        self.fontSize = self.menuSize * 0.6
         self.backGround = p.transform.scale(self.cache["mainMenu/bg"], (self.lowestSize, self.lowestSize))
         self.textBackground = pyr_pg.ui.create_textbox((10,9), self.menuSize, self.cache["textbox/background/menu"])
         self.backButton = pyr_pg.ui.button(self.window, (5, 9.25), self.menuSize, (3, 1), self.cache["buttons/defaultBackground"], fontSystem=self.font, text="Back")
+        self.__renderTextToTextbox()
 
     def main_loop(self) -> tuple[None | str, None | int, None]:
         '''Creating and handling of the main menu'''
@@ -51,7 +53,12 @@ class infoMenu:
         for line in infoFile.readlines():
             info.append(line.strip())
         infoFile.close()
-        return 
+        return info
         
     def __renderTextToTextbox(self):
+        self.font.draw("Info", self.fontSize, (self.menuSize * 5.05, self.menuSize * 1.55), placement=4, color=(0, 0, 0), blitSurface=self.textBackground)
+        self.font.draw("Info", self.fontSize, (self.menuSize * 5, self.menuSize * 1.5), placement=4, color=(200, 200, 200), blitSurface=self.textBackground)
+        for n in range(len(self.textList)):
+            self.font.draw(self.textList[n], self.fontSize * 0.5, (self.menuSize * 5.03, self.menuSize * (2.53 + (n * 0.4))), placement=4, color=(0, 0, 0), blitSurface=self.textBackground)
+            self.font.draw(self.textList[n], self.fontSize * 0.5, (self.menuSize * 5, self.menuSize * (2.5 + (n * 0.4))), placement=4, color=(200, 200, 200), blitSurface=self.textBackground)
         pass
