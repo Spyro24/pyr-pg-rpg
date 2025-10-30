6144a4234dcffe04ca39718e36901555a09b8f56
Spyro24 <minerpi16@gmail.com>
2025-10-30 22:22:25 +0100

    adding the barebone code to make the map syystem work.(we are going forward now)

--- manor/main_game.py
+++ manor/main_game.py
@@ -12,11 +12,11 @@ class game:
         self.mapSystem = container.mapSystem
         self.lastFunction = None
         self.windowHasResized = False
+        self.perlinNoise = pyr_pg.math_.PerlinNoise2D(seed=255)
         self.setup()
         
     def setup(self):
         self.lowestSize = self.window.lowestSize
-        self.menuSize = self.lowestSize / 10
     
     def main_loop(self) -> tuple[None | str, None | int, None]:
         '''Creating and handling of the main menu'''
@@ -28,10 +28,20 @@ class game:
         return (None, None, None)
     
     def render(self):
-        pass
+        for y in range(16):
+            for x in range(16):
+                noise = self.perlinNoise.noise(x*0.1, y*0.1)
+                if noise < 0.4:
+                    self.mapSystem.blit_tile("ground", 5, (x,y))
+                elif noise < 0.6:
+                    self.mapSystem.blit_tile("ground", 4, (x,y))
+                else:
+                    self.mapSystem.blit_tile("ground", 1, (x,y))
+        self.window.blit(p.transform.scale(self.mapSystem.ground, (self.lowestSize, self.lowestSize)))
     
     def debug_render(self):
         pass
     
     def on_window_update(self):
         self.windowHasResized = False
+        self.setup()
--- pyr_pg/map_system.py
+++ pyr_pg/map_system.py
@@ -12,12 +12,28 @@ class mapSystem:
                       "playerOveroverlay": [],
                       "shadows": [],
                       }
-        self.ground = p.Surface((self.mapSize[0], self.mapSize[1]))
+        self.ground = p.Surface((self.mapSize[0] * self.tileSize, self.mapSize[1] * self.tileSize))
+        self.mapLayers = {"ground": self.ground,
+                          }
         self.mapXPos = 0
         self.mapXPos = 0
         
     def load_tiles(self, paths={"ground": "./res/tiles/ground"}):
+        """Load Every tile in the tile paths"""
         for path in paths.keys():
             tileInternalPath = path
             tileExternalPath = paths[path]
             sheetCount = len(os.listdir(tileExternalPath))
+            for sheetNumber in range(sheetCount):
+                sheet = p.image.load(f"{tileExternalPath}/{sheetNumber}.png")
+                for y in range(self.tileSheetSize[1]):
+                    for x in range(self.tileSheetSize[0]):
+                        tile = p.Surface((self.tileSize, self.tileSize), p.SRCALPHA)
+                        tile.blit(sheet, (-(x * self.tileSize), -(y * self.tileSize)))
+                        self.tiles[tileInternalPath].append(tile.convert_alpha())
+    
+    def blit_tile(self, layer: str, tileId: int, pos: tuple[int, int]):
+        """Blit a single tile to a layer of the map with the tile ID"""
+        if tileId > 0:
+            tileId = tileId - 1
+            self.mapLayers[layer].blit(self.tiles[layer][tileId], (pos[0] * self.tileSize, pos[1] * self.tileSize))
